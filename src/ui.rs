3d4f5385b4a6792ca9f310daa1cc2abde6b51e40
CalebKornegay <caleb.kornegay@gmail.com>
2025-06-11 21:45:32 -0400

    Added colors as args

--- src/ui.rs
+++ src/ui.rs
@@ -3,23 +3,35 @@ use ratatui::{layout::{Constraint, Direction, Flex, Layout, Rect}, style::{Color
 pub struct Ui;
 
 impl Ui {
-    pub fn show_help<'a>(frame: &mut Frame<'a>, keybinds_text: &Vec<&str>) {
-        frame.render_widget(Paragraph::new("").block(Self::generate_block(String::from("Help"))), frame.area());
+    pub fn show_help<'a>(frame: &mut Frame<'a>, keybinds_text: &Vec<&str>, tc: Color, bgc: Color) {
+        frame.render_widget(
+            Paragraph::new("")
+                .block(
+                    Self::generate_block(
+                        String::from("Help"),
+                        Some(tc),
+                        Some(bgc)
+                    )
+            ),
+            frame.area()
+        );
+
+        let style = Style::default().fg(tc);
                     
         let mut help_text = keybinds_text.clone().iter()
             .map(|&l| {
                 if l == "[ctrl+h] help" {
-                    Line::from("[ctrl+h] to exit this screen")
+                    Line::styled("[ctrl+h] to exit this screen", style.clone())
                 } else {
-                    Line::from(l)
+                    Line::styled(l, style.clone())
                 }
             })
             .collect::<Vec<Line>>();
         help_text.extend_from_slice(
             &[
-                Line::from("[ctrl+r] reset scroll"),
-                Line::from("[\u{2195}] use arrow keys or mouse to move up and down"),
-                Line::from("Enter characters to fuzzy search for processes"),
+                Line::styled("[ctrl+r] reset scroll", style.clone()),
+                Line::styled("[\u{2195}] use arrow keys or mouse to move up and down", style.clone()),
+                Line::styled("Enter characters to fuzzy search for processes", style),
             ]
         );
 
@@ -42,15 +54,19 @@ impl Ui {
             .areas(area);
         area
     }
-    
-    pub fn generate_block<'a>(name: String) -> Block<'a> {
+
+    pub fn generate_block<'a>(name: String, title_color: Option<Color>, bg_color: Option<Color>) -> Block<'a> {
         Block::default()
             .title(name)
             .title_alignment(ratatui::layout::Alignment::Center)
-            .title_style(Style::default().fg(Color::Rgb(0xff, 0xff, 0xff)))
+            .title_style(Style::default().fg(
+                title_color.unwrap_or(Color::Rgb(0xff, 0xff, 0xff))
+            ))
             .borders(Borders::ALL)
             .border_style(Style::default().fg(Color::Rgb(0x3a, 0x3a, 0x3a)))
-            .style(Style::default().bg(Color::Rgb(0x12, 0x12, 0x12)))
+            .style(Style::default().bg(
+                bg_color.unwrap_or(Color::Rgb(0x12, 0x12, 0x12))
+            ))
     }
 }
 
