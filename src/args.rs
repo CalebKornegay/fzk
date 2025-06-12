8418199e673df89c4de4a7323d9fc7a8a6e491a2
CalebKornegay <caleb.kornegay@gmail.com>
2025-06-11 21:46:33 -0400

    Add options for highlight and background color, added better help for interval, threshold, and num_matches, added show_colors

--- src/args.rs
+++ src/args.rs
@@ -10,12 +10,21 @@ use clap::Parser;
 )]
 
 pub struct Args {
-    #[arg(short = 't', long, help="The similarity threshold for matching (default 0.3)")]
+    #[arg(short = 't', long, help="The similarity threshold for matching between 0.0 and 1.0 (default 0.3)")]
     pub threshold: Option<f32>,
 
-    #[arg(short = 'i', long, help="The update interval for processes in seconds (default 3)")]
+    #[arg(short = 'i', long, help="The update interval for processes in seconds (default 3, minimum 0.75)")]
     pub update_interval: Option<f32>,
 
-    #[arg(short = 'n', long, help="The maximum number of matches from fuzzy matcher (default 20)")]
-    pub num_matches: Option<usize>
+    #[arg(short = 'n', long, help="The maximum number of matches from fuzzy matcher (default 25, minimum 1)")]
+    pub num_matches: Option<usize>,
+
+    #[arg(short = 'c', long, help="The color of the highlighted process (default lightblue)")]
+    pub highlight_color: Option<String>,
+
+    #[arg(short = 'b', long, help="The background color of the entire interface (default 0x12, 0x12, 0x12)")]
+    pub background_color: Option<String>,
+
+    #[arg(long, help="Show colors")]
+    pub show_colors: bool
 }
