8618bee3b164573c5e3f32a14c0c0e45ed51ce49
CalebKornegay <caleb.kornegay@gmail.com>
2025-06-11 21:44:50 -0400

    Remove testing code from prior

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,6 @@ use std::io::stdout;
 use std::error::Error;
 use scopeguard::defer;
 
-use crate::interface::{ProcessMonitor, Monitor};
-
 use app::App;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -43,43 +41,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     if let Err(err) = res {
         eprintln!("{}", err.to_string());
     }
-
-    // use clap::Parser;
-    // let args = crate::args::Args::parse();
-
-    // let mut monitor = Monitor::new(
-    //     args.update_interval.unwrap_or(3.0),
-    //     args.threshold.unwrap_or(0.4)
-    // );
-
-    // monitor.get_procs_from_system();
-
-    // if let Some(res) = monitor.get_all_procs() {
-    //     res.iter().for_each(|proclist| {
-    //         proclist.iter().for_each(|proc| {
-    //             println!("{} {} {} {}", proc.get_command(), proc.get_pid(), proc.get_mem(), proc.get_cpu());
-    //         })
-    //     })
-    // }
-
-    // if let Some(chrome) = monitor.current_procs.get("chrome.exe") {
-    //     println!("found chrome, pids = {}", 
-    //         chrome.iter().map(|p| p.get_pid().to_string()).collect::<Vec<String>>().join(" ")
-    //     );
-    // }
-
-    // let res = monitor.current_procs.get("chrome.exe");
-    // if let Some(chrome) = res {
-    //     chrome.iter().for_each(|p| Monitor::kill_proc(p));
-    // }
-
-    // monitor.kill_proc_list("chrome.exe");
-
-    // if let Some(chrome) = monitor.current_procs.get("chrome.exe") {
-    //     println!("found chrome, pids = {}", 
-    //         chrome.iter().map(|p| p.get_pid().to_string()).collect::<Vec<String>>().join(" ")
-    //     );
-    // }
 
     Ok(())
 }
